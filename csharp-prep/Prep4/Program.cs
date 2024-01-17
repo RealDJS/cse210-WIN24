0e1dc9342f0d131f6053922698a83a00e178b27d
CodeBreaker <dj@skoys.com>
2024-01-16 22:41:23 -0700

    Completed Prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Hello Prep4 World!");// Greeting Message
+
+        // Create List
+        List<int> numbers = new List<int>();
+
+        // Variables
+        int userInput = -1;
+
+        // Numbered List
+        while (userInput != 0)
+        {
+            // User Enters Numbers
+            Console.Write("Enter a number: ");
+
+            string input = Console.ReadLine();// stores input to variable
+
+            userInput = int.Parse(input);// converts string to int
+
+            if (userInput != 0)
+            {
+                numbers.Add(userInput);// integers are added to list
+            }
+        };
+
+
+        // Compute Sum
+        int sum = 0;
+        foreach (int i in numbers)// prints each number
+        {
+            sum += i;
+        }
+
+
+        // Compute Average
+        int items = numbers.Count;
+        float average = ((float)sum) / items;// average is the total divided by the number of items
+
+        // Biggest Number
+        int biggestNumber = numbers[0];
+
+        // Iterates through List
+        foreach (int i in numbers)
+        {
+            if (i > biggestNumber)
+            {
+                biggestNumber = i;// changes to larger number
+            }
+        }
+
+        // Print Results
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {biggestNumber}");
     }
 }
