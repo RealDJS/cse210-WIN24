3a990455c78cb059ba722537c55debdf5cf2af70
CodeBreaker <dj@skoys.com>
2024-02-28 16:50:46 -0700

    Develop04 work

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,15 @@
-using System;
+// Develop04 Source Code
+// Dennis Skoy
+// Last edit: Feb 2024
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+        // (hard to do, but maybe) make a list/dictionary for the program to iterate through instead of a menu
+
+        Activity activity = new();
+        activity.RunActivity();
     }
 }
